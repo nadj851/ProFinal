8f1eb67661b8a22f0257fb22f22217b7791c74d7
e1695946 <e1695946@FC413-G3MFX12>
2018-05-03 14:55:11 -0400

    rien

---
+++ Enchere/Enchere/Migrations/201805011442344_coucou.cs
@@ -0,0 +1,27 @@
+namespace Enchere.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class coucou : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.tests");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.tests",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nom = c.String(),
+                        Prenom = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
