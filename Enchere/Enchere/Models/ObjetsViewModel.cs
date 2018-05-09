5ec69af800d2fddabbf7e854834f388653465e37
MousKamel <31279431+MousKamel@users.noreply.github.com>
2018-05-09 12:33:19 -0400

    NouveauViewModel pour Index

---
+++ Enchere/Enchere/Models/ObjetsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enchere.Models
+{
+    public class ObjetsViewModel
+    {
+        public IList<Categorie> Categorie { get; set; }
+        public IList<Encheree> Enchere { get; set; }
+    }
+}
