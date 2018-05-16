ebcfe8c4450555553ce811fbf7c379be52c2eecb
Lahcene <lahcene.maredj@gmail.com>
2018-05-16 10:50:18 -0400

    probleme usertype membre réglé

---
+++ Enchere/Enchere/Migrations/201805161345028_emailChange_DureeChange.cs
@@ -0,0 +1,16 @@
+namespace Enchere.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class emailChange_DureeChange : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
