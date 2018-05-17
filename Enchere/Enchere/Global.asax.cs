ff5cb130f21a022be8094e4c9743b64a43631e55
reyderrs <reyderrs@gmail.com>
2018-05-17 15:24:02 -0400

    Timer rajouté dans le fichier Global.asax.cs.

--- Enchere/Enchere/Global.asax.cs
+++ Enchere/Enchere/Global.asax.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static ApplicationDbContext db = new ApplicationDbContext();
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
         }
+
+
+
+        // Specify what you want to happen when the Elapsed event is raised.
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            var list = db.Encherees.ToList();
+            foreach (var item in list)
+            {
+                System.Diagnostics.Debug.WriteLine(item.objet.objetNom);
+            }
+            
+            
+        }
+
+
     }
 }
