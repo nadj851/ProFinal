f1edd34e1ab49ee4d02408cd866b49476aad0b0d
nadj851 <nadjiblar@hotmail.fr>
2018-05-17 17:42:54 -0400

    Update ObjetsController.cs

--- Enchere/Enchere/Controllers/ObjetsController.cs
+++ Enchere/Enchere/Controllers/ObjetsController.cs
@@ -221,6 +221,116 @@ namespace Enchere.Controllers
            
         }
 
+  // GET: Evaluations/Create
+        public ActionResult CreateEval(int objetId)
+        {
+             Session["ObjetId"]= objetId;
+            var ObjetId = (int)Session["ObjetId"];
+
+            var obj = db.Objets.Where(a => a.Id == objetId).Single();
+            ViewBag.Vendeur = obj.User.UserName;
+
+            ViewBag.Name = User.Identity.Name;
+            //ViewBag.UserId = new SelectList(db.Users, "Id", "Civilite");
+            return View();
+        }
+
+
+
+        // POST: Evaluations/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEval([Bind(Include = "Id,DateEvaluation,Cote,Commentaire,Vendeur,UserId")] Evaluation evaluation)
+        {
+            if (ModelState.IsValid)
+            {
+                evaluation.UserId = User.Identity.GetUserId();
+                evaluation.DateEvaluation = DateTime.Now;
+
+                var ObjetId = (int)Session["ObjetId"];
+
+                var obj = db.Objets.Where(a => a.Id == ObjetId).Single();
+                evaluation.Vendeur = obj.User.UserName;
+
+                //check sum cote
+                double total = 0;
+
+                //On regarde si il y a une evaluation existante pour l'utilisateur donné
+                if (db.Evaluations.Where(a => a.Vendeur == obj.User.UserName).Any())
+                {
+                    total = (from eval in db.Evaluations
+
+                             where eval.Vendeur == obj.User.UserName
+                             select eval.Cote).Sum();
+
+                }
+
+                evaluation.TotalCote = total + evaluation.Cote;
+
+                //send notification
+
+
+                ViewBag.total = evaluation.TotalCote;
+                db.Evaluations.Add(evaluation);
+                db.SaveChanges();
+                if (evaluation.TotalCote <= -6)
+                {
+                    envoiMail(obj.User);
+                }
+                if (evaluation.TotalCote <= -9)
+                {
+                    var UserId = User.Identity.GetUserId();
+                    var u = db.Users.Where(a => a.Id == obj.User.Id).Single();
+
+                    //pour desactiver un compte
+                    //https://stackoverflow.com/questions/30452104/mvc-5-identity-2-0-lockout-doesnt-work
+                    //Il faut mettre a jour la colone lockout enable et donner une date jusqua quand ce sera bloquer sinon sa ne marchera pas
+                    u.LockoutEnabled = true;
+                    u.LockoutEndDateUtc = new DateTime(DateTime.Now.AddDays(7).Ticks);
+
+
+
+                }
+
+                return RedirectToAction("Index","Evaluations");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Civilite", evaluation.UserId);
+            return View(evaluation);
+        }
+
+
+        private void envoiMail(ApplicationUser user)
+        {
+            SmtpClient SmtpServer = new SmtpClient("smtp.live.com");
+            var mail = new MailMessage();
+            //recherche de la cote actuelle
+            var coteUser = db.Evaluations.OrderByDescending(p => p.Id).FirstOrDefault().TotalCote;
+
+            mail.From = new MailAddress("munarela@hotmail.com");
+            mail.To.Add(user.Email);
+            mail.Subject = "Urgent";
+            mail.IsBodyHtml = true;
+            //le message du body
+            string body = "Nom expéditeur: " + "admin" + "<br>" +
+                "email expéditeur: " + "munarela@hotmail.com" + "<br>" +
+                "objet de message: " + "Cote critique " + "<br>" +
+                "le message pour " + user.UserName + ": <b>Nous vous informons que votre cote à atteint le seuil critique de " + coteUser;
+
+            mail.Body = body;
+
+            SmtpServer.Port = 587;
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Credentials = new NetworkCredential("munarela@hotmail.com", "Web123456");
+            SmtpServer.EnableSsl = true;
+            SmtpServer.Send(mail);
+
+
+
+        }
+
 
 
     }
